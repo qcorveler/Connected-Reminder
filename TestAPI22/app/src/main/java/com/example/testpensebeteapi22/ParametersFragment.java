2dc7c5514927b985e090ee642849566464129789
jbrunet <brunetjerem@hotmail.fr>
2024-04-08 10:42:28 +0200

    Ajout des paramètres déjà séléctionnés

--- TestAPI22/app/src/main/java/com/example/testpensebeteapi22/ParametersFragment.java
+++ TestAPI22/app/src/main/java/com/example/testpensebeteapi22/ParametersFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -20,10 +21,25 @@ public class ParametersFragment extends Fragment {
     Switch isHourVisible;
     Switch sounds;
     Spinner police_size;
+    Button save;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_parameters,container, false);
+
+        notifications = rootView.findViewById(R.id.notifications);
+        standby = rootView.findViewById(R.id.standbymode);
+        isPastAccessible = rootView.findViewById(R.id.pastaccessible);
+        isHourVisible = rootView.findViewById(R.id.hourvisible);
+        police_size = rootView.findViewById(R.id.police);
+        save = rootView.findViewById(R.id.add);
+
+        save.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(rootView.getContext(), "Paramètres sauvegardés", Toast.LENGTH_LONG).show();
+            }
+        });
         return rootView;
-}}
+    }}
