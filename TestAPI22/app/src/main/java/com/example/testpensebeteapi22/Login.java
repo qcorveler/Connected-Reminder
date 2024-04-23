7a39fa3853ef32f5ec9ef56a6b0d9bf7b785bbf4
jbrunet <brunetjerem@hotmail.fr>
2024-04-23 11:25:41 +0200

    Login et Register fonctionnel avec sauvegarde des infos dans la base de données

--- TestAPI22/app/src/main/java/com/example/testpensebeteapi22/Login.java
+++ TestAPI22/app/src/main/java/com/example/testpensebeteapi22/Login.java
@@ -1,5 +1,6 @@
 package com.example.testpensebeteapi22;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,7 +11,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Login extends AppCompatActivity {
+
+    DatabaseReference database = FirebaseDatabase.getInstance("https://pense-bete-9293d-default-rtdb.europe-west1.firebasedatabase.app").getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +41,32 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(Login.this, "Veuillez entre votre email ou mot de passe", Toast.LENGTH_SHORT).show();
                 }
                 else{
+                    database.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            // On vérifie si un id correspond
+                            for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                                String id = childSnapshot.getKey();
+                                String getPwd = snapshot.child(id).child("password").getValue(String.class);
+                                String getEmail = snapshot.child(id).child("email").getValue(String.class);
+                                if(getEmail.equals(emailTxt) && getPwd.equals(passwordTxt)){
+                                    Toast.makeText(Login.this, "Connexion réussie", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Login.this, MainMenuActivity.class);
+                                    intent.putExtra("idConnexion",id);
+                                    finish();
+                                    break;
+                                }
+                                else{
+                                    Toast.makeText(Login.this, "Email/Password inconnus", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
 
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
             }
         });
--- TestAPI22/app/src/main/java/com/example/testpensebeteapi22/MainMenuActivity.java
+++ TestAPI22/app/src/main/java/com/example/testpensebeteapi22/MainMenuActivity.java
@@ -11,6 +11,7 @@ public class MainMenuActivity extends AppCompatActivity {
 
     Button helperInterface;
     Button helpedInterface;
+    int idConnexion;
 
     // Menu principal permettant de choisir quel mode on souhaite utiliser (Helper / Helped)
     @Override
@@ -20,6 +21,8 @@ public class MainMenuActivity extends AppCompatActivity {
 
         helpedInterface = findViewById(R.id.helped_button);
         helperInterface = findViewById(R.id.helper_button);
+        idConnexion = getIntent().getIntExtra("idConnexion", -1); // -1 est une valeur par défaut si l'extra n'est pas trouvé
+        setIdConnexion(idConnexion); // il faudra propager cet id pour savoir de quel utilisateur on parle
 
         helperInterface.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -38,5 +41,9 @@ public class MainMenuActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void setIdConnexion(int idConnexion) {
+        this.idConnexion = idConnexion;
     }
 }
